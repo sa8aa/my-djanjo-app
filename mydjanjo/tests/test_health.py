931de9401f0db589ee4ce5cd7e28a71f36312b92
Samar <samar8brm@gmail.com>
2026-04-26 14:29:51 +0100

    added tests/__init__.py and tests/test_health.py

---
+++ mydjanjo/tests/test_health.py
@@ -0,0 +1,7 @@
+from django.test import TestCase, Client
+
+class HealthCheckTest(TestCase):
+    def test_health_endpoint(self):
+        c = Client()
+        response = c.get("/health/")
+        self.assertEqual(response.status_code, 200)
