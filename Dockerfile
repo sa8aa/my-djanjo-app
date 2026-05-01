1002fab36cb47d56e64289b2adb4edb9f34d0f47
sa8aa <sarrah58day@gmail.com>
2026-05-01 11:31:06 +0100

    Add cloudformation infra stack and update Dockerfile and settings

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,9 @@
 FROM python:3.12-slim
-
 WORKDIR /app
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
-
 COPY . .
-
+ENV DJANGO_SETTINGS_MODULE=mydjanjo.settings
 RUN python manage.py collectstatic --noinput
-
 EXPOSE 8000
-CMD ["gunicorn", "myproject.wsgi:application", "--bind", "0.0.0.0:8000"]
+CMD ["gunicorn", "mydjanjo.wsgi:application", "--bind", "0.0.0.0:8000"]
--- mydjanjo/settings.py
+++ mydjanjo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -49,8 +49,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mydjanjo.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -65,9 +63,8 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'mydjanjo.wsgi.application'
-
+ROOT_URLCONF = 'mydjanjo.urls'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
@@ -115,7 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
