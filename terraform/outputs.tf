a7e8bf7570797700b7f098fc6af384416d13ee6e
Samar Ben Romdhane <samar8brm@gmail.com>
2026-05-02 12:03:05 +0100

    Delete terraform directory

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "ecr_repo_url" {
-  value = aws_ecr_repository.app.repository_url
-}
-
-output "db_endpoint" {
-  value = aws_db_instance.postgres.endpoint
-}
--- terraform/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "aws_region"   { default = "us-east-1" }
-variable "app_name"     { default = "django-app" }
-variable "db_name"      { default = "mydb" }
-variable "db_user"      { default = "myuser" }
-variable "db_password"  { sensitive = true }
